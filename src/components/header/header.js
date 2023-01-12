221cb61e7e8a2f94fb1ca53e7dae70183a2203f6
Nick Reynolds <nick.reynolds@revunit.com>
2023-01-12 15:51:10 -0800

    add disney cards to repo

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import "../styles/header.scss"
+
+export const Header = ({ setSearchTerm }) => {
+    return (
+        <nav className="navbar">
+            <div className="container-fluid px-4">
+                <div className="navbar-brand">
+                    <div className="title">Who Dis-ney</div>
+                </div>
+                <form className="d-flex">
+                    <input className="form-control" type="search" placeholder="Search Name" aria-label="Search" onChange={(e) => {setTimeout(() => { setSearchTerm(e.target.value) }, 500) }} />
+                </form>
+            </div>
+        </nav>
+    )
+}
---
+++ src/components/home/home.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import { Header } from "../header/header"
+import { CharacterCards } from "../character-card/character-card"
+
+export const Home = () => {
+    const [searchTerm, setSearchTerm] = useState("")
+    const [sort, setSort] = useState(false)
+
+    return ( 
+        <>
+            <Header setSearchTerm={setSearchTerm} />
+            <div className="container">
+                <button type="button" className="btn btn-outline-secondary mb-3" onClick={() => setSort(!sort)}>{sort ? "Sort Alphabetically" : "Sort Reverse"}</button>
+                <div className="row">
+                    <CharacterCards searchTerm={searchTerm} sort={sort} />
+                </div>
+            </div>
+        </>
+    )
+}
