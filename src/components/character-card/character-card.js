72919a7736cb78d36b8b54cb810e829086ae8038
Nick Reynolds <nick.reynolds@revunit.com>
2023-01-12 16:14:40 -0800

    code cleanup

--- src/components/character-card/character-card.js
+++ src/components/character-card/character-card.js
@@ -18,7 +18,6 @@ export const CharacterCards = ({ searchTerm, sort }) => {
 					console.log('Request Failed:', e);
 				}
 			}
-
 		}
 		getCharacters();
 	}, [searchTerm])
@@ -35,15 +34,14 @@ export const CharacterCards = ({ searchTerm, sort }) => {
 					console.log('Request Failed:', e);
 				}
 			}
-
 		}
-		getSearchedCharacter();
+    getSearchedCharacter();
 	}, [searchTerm]);
 
 
 	const sortAlpha = (arr) => {
 		arr.sort((a, b) => {
-			const nameA = a.name.toUpperCase();
+      const nameA = a.name.toUpperCase();
 			const nameB = b.name.toUpperCase();
 
 			if (nameA > nameB) return -1;
@@ -55,7 +53,7 @@ export const CharacterCards = ({ searchTerm, sort }) => {
 
 	const sortReverse = (arr) => {
 		arr.sort((a, b) => {
-			const nameA = a.name.toUpperCase();
+      const nameA = a.name.toUpperCase();
 			const nameB = b.name.toUpperCase();
 
 			if (nameA < nameB) return -1;
@@ -68,50 +66,50 @@ export const CharacterCards = ({ searchTerm, sort }) => {
 	sort ? sortAlpha(disneyCharacters) : sortReverse(disneyCharacters);
 
 	return (
-        disneyCharacters.length ?
-            disneyCharacters.map((character, i) => {
-				return (
-                    <div className="col-sm-6 col-lg-4 mb-4" key={i}>
-                        <div className="card h-100  border-secondary">
-                            <img src={character.imageUrl} className="card-img-top card-img" alt="disney character" />
-                            <div className="card-body">
-                                <h2 className="card-title">{character.name}</h2>
-                                <ul className="list-group">
-                                    <li className="list-group-item">
-                                        <div><strong>Featured Films:</strong></div>
-                                        {
-											character.films.length ? character.films.map((film, i) => {
-                                                return (
-                                                    <div key={i}>{film}</div>
-                                                )
-                                            })
-                                            : "Not featured in any Disney movies"
-                                        } 
-                                    </li>
-                                    <li className="list-group-item">
-                                        <div><strong>Featured TV Shows:</strong></div>
-                                        {
-                                            character.tvShows.length ? character.tvShows.map((shows, i) => {
-                                                return (
-                                                    <div key={i}>{shows}</div>
-                                                )
-                                            })
-                                            : "Not featured in any Disney TV shows"
-                                        } 
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                )
-			})
-			: 
-			<div className="col-xs-12">
-				<div className="d-flex justify-content-center">
-					<div className="spinner-border text-secondary" role="status">
-						<span className="visually-hidden">Loading...</span>
-					</div>
-				</div>
-			</div>
-    )
+    disneyCharacters.length ?
+    disneyCharacters.map((character, i) => {
+      return (
+        <div className="col-sm-6 col-lg-4 mb-4" key={i}>
+          <div className="card h-100  border-secondary">
+            <img src={character.imageUrl} className="card-img-top card-img" alt="disney character" />
+            <div className="card-body">
+              <h2 className="card-title">{character.name}</h2>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <div><strong>Featured Films:</strong></div>
+                  {
+                    character.films.length ? character.films.map((film, i) => {
+                      return (
+                        <div key={i}>{film}</div>
+                      )
+                    })
+                    : "Not featured in any Disney movies"
+                  } 
+                </li>
+                <li className="list-group-item">
+                  <div><strong>Featured TV Shows:</strong></div>
+                  {
+                    character.tvShows.length ? character.tvShows.map((shows, i) => {
+                      return (
+                        <div key={i}>{shows}</div>
+                      )
+                    })
+                    : "Not featured in any Disney TV shows"
+                  } 
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )
+    })
+    : 
+    <div className="col-xs-12">
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border text-secondary" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,18 +3,18 @@ import { Header } from "../header/header"
 import { CharacterCards } from "../character-card/character-card"
 
 export const Home = () => {
-    const [searchTerm, setSearchTerm] = useState("")
-    const [sort, setSort] = useState(false)
+  const [searchTerm, setSearchTerm] = useState("")
+  const [sort, setSort] = useState(false)
 
-    return ( 
-        <>
-            <Header setSearchTerm={setSearchTerm} />
-            <div className="container">
-                <button type="button" className="btn btn-outline-secondary mb-3" onClick={() => setSort(!sort)}>{sort ? "Sort Alphabetically" : "Sort Reverse"}</button>
-                <div className="row">
-                    <CharacterCards searchTerm={searchTerm} sort={sort} />
-                </div>
-            </div>
-        </>
-    )
+  return ( 
+    <>
+      <Header setSearchTerm={setSearchTerm} />
+      <div className="container">
+        <button type="button" className="btn btn-outline-secondary mb-3" onClick={() => setSort(!sort)}>{sort ? "Sort Alphabetically" : "Sort Reverse"}</button>
+        <div className="row">
+          <CharacterCards searchTerm={searchTerm} sort={sort} />
+        </div>
+      </div>
+    </>
+  )
 }
